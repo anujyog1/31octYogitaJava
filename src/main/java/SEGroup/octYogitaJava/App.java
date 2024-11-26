591fd8908f91ec0667bf191b84dada9e1b6ab733
anujyog1 <66674832+anujyog1@users.noreply.github.com>
2024-11-26 22:55:29 +0530

    Update App.java

--- src/main/java/SEGroup/octYogitaJava/App.java
+++ src/main/java/SEGroup/octYogitaJava/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello all today is 13 Nov 2024" );
+        System.out.println( "Hello all today is 13 Nov 2024_____edited on 26 nov 2024   " );
     }
 }
